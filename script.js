d9a486a8e458dccc0fa362b9a8ad049732fe5dbe
Giorgi Bokuchava <113925720+Boku52500@users.noreply.github.com>
2022-10-24 22:06:18 +0400

    Add plain HTML and Collection function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const variable = {
+  title : [],
+  author : [],
+}
 
+function addBook(title, author) {
+  variable['title'] = title;
+  variable['author'] = author;
+  return variable;
+}
+
+addBook('Harry Potter', 'J.K Rowling');
+addBook('Hello', 'helloo');
